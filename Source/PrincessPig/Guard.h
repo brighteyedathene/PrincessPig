5b587a3d687cbca251ba297887398ec19f73bff9
brighteyedathene <brighteyedathene@gmail.com>
2019-01-28 02:58:09 +0000

    added new classes and blueprints to put ai code in

---
+++ Source/PrincessPig/Guard.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PrincessPigCharacter.h"
+#include "Guard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRINCESSPIG_API AGuard : public APrincessPigCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
