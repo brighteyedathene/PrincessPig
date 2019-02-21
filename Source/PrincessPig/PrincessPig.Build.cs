639d16bdca88a904c4d86f1d1474c50165f711c3
brighteyedathene <brighteyedathene@gmail.com>
2019-02-21 17:16:58 +0000

    Net/lobby (#10)
    * added new section + split up navmesh
    
    * added wandering guards outside
    
    * moved maps directory, added loads of empty folders and blueprints while doing the setup for multiplayer lobbies
    
    * added thumbnail images, some player info stuff for lobby tutorial 04
    
    * did the main menu widget, as per lobby tutorial 05
    
    * new icons, filled out lobby tutorial up to 11
    
    * fixed missing binding on LAN/Internet toggle
    
    * set ServerMenu's TimerValue default to 10, moved a few nodes around
    
    * fixed network type toggle buttons on server menu
    
    * added debug lines after join session
    
    * filled out network + travel error printing nodes
    
    * set default online platform service to lan in ini
    
    * reverted default platform service to steam in ini
    
    * added a skybox to main menu
    
    * added some debug info after launching lobby on server
    
    * made internet the default network type and disconnected the buttons to change it
    
    * tutorial'd as far as number 13, lobby menu is blocked out but not event-graphed
    
    * lobby menu, up to number 15
    
    * filled out player card, won't show for host yet...
    
    * added a Delay->EveryoneUpdate at the end of OnPostLogin in  LobbyGM so fix the host card not appearing
    
    * added kick menu
    
    * fixed missing property on create player button widget
    
    * accounted for edge case where kick player menu is open and a play joins
    
    * restart level attempt - doesn't work locally so far
    
    * restart level attempt - using console command with server's player controller from within the level blueprint
    
    * enabled seamless travel in GameplayGM in an attempt to make restart-level work
    
    * added character select + a couple of new images for hover/clicked for princess
    
    * added chat
    
    * finished off tutorial stuff
    
    * maybe fixed character image bug
    
    * adjusted the player list's appearance to split up player and character icons
    
    * host and join can be traversed by pressing enter on text box, fixed warning about character select widget focus

--- Source/PrincessPig/PrincessPig.Build.cs
+++ Source/PrincessPig/PrincessPig.Build.cs
@@ -9,5 +9,7 @@ public class PrincessPig : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "GameplayTasks" });
+
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
     }
 }
